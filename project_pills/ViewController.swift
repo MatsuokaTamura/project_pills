eba3cfdb304e081b55f79e8e31bb2aec77523c09
matsuoka_tamura <sam-anas2@yandex.ru>
2022-02-28 10:42:52 +0300

    Initial Commit

---
+++ project_pills/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project_pills
+//
+//  Created by Matsuoka Tamura on 28.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
